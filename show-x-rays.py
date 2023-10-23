c1b30148563a1b44d9e7c9da8f44ea5bdb59e5b9
Wesley Stevick <12481632w@gmail.com>
2023-10-23 13:48:53 -0400

    fix bug in show-x-rays.py

--- show-x-rays.py
+++ show-x-rays.py
@@ -15,7 +15,7 @@ hist_data = data["hist_data"]
 # the next command line argument says which to use
 plotnum = int(sys.argv[2])
 heights, errors = hist_data[plotnum]
-source_energy = energies[plotnum]
+source_energy = energies[plotnum] * 1e3
 
 # work out the simulation paramaters (in KeV)
 max_energy = max(energies) * 1e3
@@ -34,7 +34,7 @@ plt.bar(xs, heights, width=bin_size)
 # plot visible X-ray spikes
 x_rays = [
     (11.371, "L3N3"),
-    (11.92778, "L3 edge"),
+    (11.6105, "L1M3"),
     (66.993, "KL2"),
     (68.8069, "KL3"),
     (77.57730000000001, "KM2"),
@@ -45,7 +45,7 @@ x_rays = [
 
 for x_ray, name in x_rays:
     # convert energy to KeV, then calculate the position of the spike caused by the X-ray
-    pos_on_plot = max_energy - x_ray
+    pos_on_plot = source_energy - x_ray
     plt.annotate(
         name, (pos_on_plot, heights[np.argmin(np.abs(xs - pos_on_plot))])
     )
