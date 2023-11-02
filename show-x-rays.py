b231bb2b15d67247a3fd609704630f1783b3b792
Wesley Stevick <12481632w@gmail.com>
2023-11-02 14:42:08 -0400

    add rightmost (l?) section of plotted x-rays

--- show-x-rays.py
+++ show-x-rays.py
@@ -35,7 +35,32 @@ plt.errorbar(bin_centers, heights, errors, 0, fmt="none", color="red")
 plt.fill_between(xs, np.append(heights, -1), step="post")
 
 # plot visible X-ray spikes
-x_rays = []
+x_rays = [
+    ("L2M1", 10.3082),
+    ("L1M1", 10.9287),
+    ("L2M3", 10.99),
+    ("L1M2", 11.2048),
+    ("L3N1", 11.1564),
+    ("L2M4", 11.442),
+    ("L3N2", 11.2755),
+    ("L3N3", 11.371),
+    ("L1M3", 11.6105),
+    ("L2M5", 11.5272),
+    ("L3N4", 11.564),
+    ("L3N5", 11.58212),
+    ("L3N6", 11.8287),
+    ("L3N7", 11.8323),
+    ("L3 edge", 11.92778),
+    ("L2N2", 13.0894),
+    ("L2N3", 13.18489),
+    ("L2N4", 13.3779),
+    ("L2N5", 13.39601),
+    ("L1N1", 13.5908),
+    ("L1N2", 13.71),
+    ("L2N6", 13.6425),
+    ("L2N7", 13.6462),
+    ("L2 edge", 13.74167),
+]
 
 for name, x_ray in x_rays:
     # convert energy to KeV, then calculate the position of the spike caused by the X-ray
