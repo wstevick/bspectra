2f22dad59a05a8e816464fe06fedffd8fd80c492
Wesley Stevick <12481632w@gmail.com>
2023-11-02 11:46:30 -0400

    simplify display-matrix.py

--- display-matrix.py
+++ display-matrix.py
@@ -7,7 +7,9 @@ import sys
 import numpy as np
 import pygame
 
-k = math.log10(10)
+# this controls how much lower values are scaled up
+# for k = log 10, 0.1 becomes 1/2, 0.01 becomes 1/3, 0.001 becomes 1/4, etc.
+k = math.log(10)
 
 
 # input from 0 to 1, output RGB triplet
@@ -20,38 +22,15 @@ to_color = np.vectorize(get_color, signature="()->(3)")
 
 
 with open(sys.argv[1], "rb") as f:
-    matrix, errors = pickle.load(f)
+    matrix, _ = pickle.load(f)
 
-# need to transpose the matricies, because of how pygame handles arrays
-# also, this way the first axis is the histogram id, which is also more convienent
+# need to transpose the matrix, because of how pygame handles arrays
+# also, this way the first axis is the histogram id, which is also more convenient
 matrix = matrix.T
-errors = errors.T
-
-# calculate the relative errors, and handle 0/0 situations
-zeros = errors == 0
-rel_errors = errors / matrix
-rel_errors[zeros] = 0
-assert np.all(rel_errors != np.nan)
-
-# calculate the maximum nonzero relative error for each experimant
-max_rel_error_idxs = np.argmax(rel_errors, axis=1)
-
-# normalize the matrix to the 0-1 range
+# normalize the matrix
 matrix /= matrix.sum(axis=1)
-
-# for each histogram, add the value with the minimum relative error to all the values
-# then, take the log10 and rescale
-for idx, max_rel_error_idx in enumerate(max_rel_error_idxs):
-    hist = matrix[idx]
-    min_value = hist[max_rel_error_idx]
-    # print(min_value, end=' ')
-    hist += min_value
-
-    hist = np.log10(hist)
-    rescale_start = np.log10(min_value)
-    rescale_stop = np.log10(1 + min_value)
-    hist = (hist - rescale_start) / (rescale_stop - rescale_start)
-    matrix[idx] = hist
+# scale the matrix to show off low values
+matrix = k / (k - np.log(matrix))
 
 # create a screen with the dimensions of the matrix, then use the color function to put the matrix on the screen
 screen = pygame.display.set_mode(matrix.shape)
