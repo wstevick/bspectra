359d5b6a3052bfef67985074ddbfeb0b08972c47
Wesley Stevick <12481632w@gmail.com>
2023-11-02 13:50:28 -0400

    label_spikes.py supports multiple x-rays in one bin

--- label_spikes.py
+++ label_spikes.py
@@ -14,22 +14,18 @@ ray_energies = np.array([float(ray[3]) for ray in (data)]) * 1e-3
 
 source_energy = float(input("Source energy of the simulation (KeV): "))
 bin_size = float(input("Bin size of the simulation (KeV): "))
-print("Enter potential spikes. EOF to exit")
-candidates = [source_energy - eval(v.strip()) for v in sys.stdin]
+print("Enter visible spikes. EOF to exit")
+visible_spikes = [source_energy - eval(v.strip()) for v in sys.stdin]
 
-found = []
-
-for idx, candidate in enumerate(candidates):
-    closest_ray = np.argmin(np.abs(candidate - ray_energies))
-
-    closest_name = ray_names[closest_ray]
-    closest_energy = ray_energies[closest_ray]
-
-    if abs(closest_energy - candidate) > bin_size / 2:
-        print(
-            f"skipping #{idx} because |{candidate} - {closest_energy}| > {bin_size / 2} for closest X-Ray {closest_name!r}"
+pprint(
+    [
+        (name, energy)
+        for spike in visible_spikes
+        for name, energy, in_bin in zip(
+            ray_names,
+            ray_energies,
+            np.abs(ray_energies - spike) <= bin_size / 2,
         )
-    else:
-        found.append((closest_name, closest_energy))
-
-pprint(found)
+        if in_bin
+    ]
+)
