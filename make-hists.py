79d66e54ea9145b4b1b277c42c656ab4ac7e1d14
Wesley Stevick <12481632w@gmail.com>
2023-11-20 14:56:29 -0500

    fix bug in make-hists.py and add modified 4-energy varients

--- make-hists.py
+++ make-hists.py
@@ -7,9 +7,9 @@ from get_histogram import get_histogram
 
 energies = [0.5, 1, 1.5, 2]
 max_energy = max(energies)
-bin_size = 1e-3
-nbins = int(max_energy / bin_size)
-ncase = 1e6
+nbins = 200
+bin_size = max_energy / nbins
+ncase = 1e7
 
 hist_data = [
     get_histogram(int(energy / bin_size), ncase, nbins, max_energy)
@@ -17,6 +17,6 @@ hist_data = [
 ]
 
 with gzip.open(
-    sys.stdout.fileno() if sys.argv[1] == "-" else sys.argv[1], "rb"
+    sys.stdout.fileno() if sys.argv[1] == "-" else sys.argv[1], "wb"
 ) as f:
     pickle.dump({"energies": energies, "hist_data": hist_data}, f)
