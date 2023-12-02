aba6d1782aa341581f584a5f891a3fc095b600ad
Wesley Stevick <12481632w@gmail.com>
2023-12-02 15:49:49 -0500

    label_spikes finds compound spikes

--- label_spikes.py
+++ label_spikes.py
@@ -20,6 +20,19 @@ visible_spikes = [source_energy - eval(v.strip()) for v in sys.stdin]
 found = []
 
 for spike in visible_spikes:
+    # iterate over every pair of spikes, counting
+    for idx1, energy1 in enumerate(ray_energies):
+        for idx2, energy2 in enumerate(ray_energies[idx1:]):
+            # determine if it's a commpound spike
+            if abs(energy1 + energy2 - spike) <= bin_size / 2:
+                # if so add the two spikes that were added to make it to the list
+                found.append(
+                    [
+                        (ray_names[idx], round(energy, 10))
+                        for (idx, energy) in [(idx1, energy1)(idx2, energy2)]
+                    ]
+                )
+
     # how far is the spike from each x-ray
     distance_to_x_rays = np.abs(ray_energies - spike)
     # which x-rays are close enough to be within the spike's bin
