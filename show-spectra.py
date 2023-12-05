0534b958e219c181aed6e37d81b28070e25420f0
Wesley Stevick <12481632w@gmail.com>
2023-12-05 10:55:27 -0500

    label show-spectra histogram and add matrix of spread out source

--- show-spectra.py
+++ show-spectra.py
@@ -11,6 +11,14 @@ for color, file in zip(colors, sys.argv[1:]):
         data = np.array(
             [[float(v) for v in line.strip().split("   ")] for line in f]
         )
-    plt.plot(data[:, 0], data[:, 1] * (data[1, 0] - data[0, 0]), color=color)
+    plt.plot(
+        data[:, 0],
+        data[:, 1] * (data[1, 0] - data[0, 0]),
+        color=color,
+        label=file,
+    )
 
+plt.xlabel("MeV")
+plt.ylabel("counts/ncase")
+plt.legend()
 plt.show()
